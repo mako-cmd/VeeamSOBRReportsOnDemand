578d819af9e7d4a8a8b1ae5c0fc6186f989967a5
mako-cmd <makomboreroman@gmail.com>
2023-08-09 14:35:04 +1000

    Initial commit

---
+++ ScaleOutEmailOnDemand.ps1
@@ -0,0 +1,8 @@
+﻿Connect-VBRServer
+$scaleout_repos = Get-VBRBackupRepository -ScaleOut
+foreach($repo in $scaleout_repos)
+{
+    Send-VBRScaleOutBackupRepositoryReport -Repository $repo
+    Write-Host "Sent email for $repo"
+}
+Disconnect-VBRServer
